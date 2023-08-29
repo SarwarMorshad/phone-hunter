0f3538fa0ec2273916e9ea23abe53d02a94be1cf
SarwarMorshad <sarwarmorshad69@gmail.com>
2023-08-30 00:09:22 +0200

    implementing Search

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-  const response = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+const loadPhone = async (searchText) => {
+  const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await response.json();
   const phones = data.data;
   //   console.log(phone);
@@ -8,8 +8,11 @@ const loadPhone = async () => {
 
 const displayPhones = (phones) => {
   const phoneContainer = document.getElementById("phone-container");
+  //   Clear Container
+  phoneContainer.textContent = "";
+
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     const phoneCard = document.createElement("div");
     phoneCard.classList = `card bg-gray-100 shadow-xl`;
     phoneCard.innerHTML = `
@@ -26,4 +29,13 @@ const displayPhones = (phones) => {
   });
 };
 
-loadPhone();
+// Handle Search button
+const handleSearch = () => {
+  //   console.log("Handle");
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  //   console.log(searchText);
+  loadPhone(searchText);
+};
+
+// loadPhone();
